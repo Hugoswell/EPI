79bbfb347662a8577438682d8c0fb0e917a54e5b
Hugo Oswell <hugo.oswell@gmail.com>
2022-02-20 17:42:18 +0100

    Improve reverse_digits code

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -2,17 +2,13 @@ from test_framework import generic_test
 
 
 def reverse(x: int) -> int:
-    negatif = x < 0
-    x = -x if negatif else x
-    res = 0
-    while x:
-        res *= 10
-        res += x % 10
-        x //= 10
-    return -res if negatif else res
+    res, x_remaining = 0, abs(x)
+    while x_remaining:
+        res = 10 * res + x_remaining % 10
+        x_remaining //= 10
+    return -res if x < 0 else res
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('reverse_digits.py',
                                        'reverse_digits.tsv', reverse))
-    # print(reverse(74236))
