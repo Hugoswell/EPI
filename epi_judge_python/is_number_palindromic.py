8c704aec594a4f6d2a2205b26e9ec71413e78388
Hugo Oswell <hugo.oswell@gmail.com>
2022-02-20 19:34:45 +0100

    Is number a palindrome

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,9 +1,19 @@
+from pickle import FALSE
 from test_framework import generic_test
 
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
+    if x < 0:
+        return False
+    low, str_int = 0, str(x)
+    high = len(str_int) - 1
+    while low < high:
+        if str_int[low] != str_int[high]:
+            return False
+        low += 1
+        high -= 1
     return True
+
 
 
 if __name__ == '__main__':
