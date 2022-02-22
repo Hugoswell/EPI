873ec93d5541795a2178c94bd3db039c84922f72
Hugo Oswell <hugo.oswell@gmail.com>
2022-02-22 22:20:08 +0100

    Did my own code for rectangle intersect

--- epi_judge_python/rectangle_intersection.py
+++ epi_judge_python/rectangle_intersection.py
@@ -13,8 +13,11 @@ def intersect_rectangle(r1: Rect, r2: Rect) -> Rect:
     def is_intersect(r1, r2):
         return (r1.x <= r2.x + r2.width and r1.x + r1.width >= r2.x
                and r1.y <= r2.y + r2.height and r1.y + r1.height >= r2.y)
-    
-    if not is_intersect(r1, r2):
+    def my_is_intersect(r1, r2):
+        return ((r2.x <= r1.x <= r2.x + r2.width or r1.x <= r2.x <= r1.x + r1.width)
+                and (r2.y <= r1.y <= r2.y + r2.height or r1.y <= r2.y <= r1.y + r1.height))
+
+    if not my_is_intersect(r1, r2):
         return Rect(0,0,-1,-1)
     return Rect(max(r1.x, r2.x), max(r1.y, r2.y),
                 min(r1.x + r1.width, r2.x + r2.width) - max(r1.x, r2.x),
