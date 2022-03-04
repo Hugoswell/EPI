b148ecde4fb8e058f5428924c7921395bdb2c500
Hugo Oswell <hugo.oswell@gmail.com>
2022-03-04 21:27:39 +0100

    Return the new list

--- epi_judge_python/dutch_national_flag_variant.py
+++ epi_judge_python/dutch_national_flag_variant.py
@@ -1,13 +1,12 @@
 from typing import List
 
-def sort_bool_values(A : List[bool]) -> None:
-    print(A)
+def sort_bool_values(A : List[bool]) -> List[bool]:
     smaller = 0
     for i in range(len(A)):
         if A[i] is False:
             A[i], A[smaller] = A[smaller], A[i]
             smaller += 1
-    print(A)
+    return A
 
 A = [True,True,False,True,False,True,True,True,False,True,False,True]
-sort_bool_values(A)
+print(sort_bool_values(A))
