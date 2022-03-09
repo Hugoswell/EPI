9d56bd0dd645a37012f268c529f290f8817a4cb4
Hugo Oswell <hugo.oswell@gmail.com>
2022-03-09 19:54:17 +0100

    4.6 done

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -3,9 +3,21 @@ from typing import List
 from test_framework import generic_test
 
 
+# def buy_and_sell_stock_once(prices: List[float]) -> float:
+#     max_profit = 0.0
+#     for i in range(len(prices)):
+#         for j in range(i+1, len(prices)):
+#             current_profit = prices[j] - prices[i]
+#             if current_profit > max_profit:
+#                 max_profit = current_profit
+#     return max_profit
+
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    min_value_so_far, max_profit = float('inf'), 0.0
+    for price in prices:
+        min_value_so_far = min(min_value_so_far, price)
+        max_profit = max(max_profit, price - min_value_so_far)
+    return max_profit
 
 
 if __name__ == '__main__':
