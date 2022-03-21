d7cb95dbf142d652ca35e2265cd72541c8a428a3
Hugo Oswell <hugo.oswell@gmail.com>
2022-03-21 21:09:06 +0100

    Try to did 5.10

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -4,8 +4,12 @@ from test_framework import generic_test
 
 
 def apply_permutation(perm: List[int], A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    B = [-1] * len(A)
+    print(B)
+    for i in range(len(A)):
+        B[perm[i]] = A[i]
+    A = B.copy()
+    print(A)
 
 
 def apply_permutation_wrapper(perm, A):
@@ -14,7 +18,8 @@ def apply_permutation_wrapper(perm, A):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('apply_permutation.py',
-                                       'apply_permutation.tsv',
-                                       apply_permutation_wrapper))
+    # exit(
+    #     generic_test.generic_test_main('apply_permutation.py',
+    #                                    'apply_permutation.tsv',
+    #                                    apply_permutation_wrapper))
+    apply_permutation([2,0,1,4,3], ['a','b','c','d','f'])
