9604b02bafcd5f991c7fa905b673d4735b2387e8
Hugo Oswell <hugo.oswell@gmail.com>
2022-04-20 21:00:30 +0200

    Code my solution for 6.1

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -15,12 +15,21 @@ def int_to_string(x: int) -> str:
             break
     return ('-' if negative else '') + ''.join(reversed(res))
 
-
+# EPI solution
 def string_to_int(s: str) -> int:
     return (-1 if s[0] == '-' else 1) * functools.reduce(
         lambda sum, c: sum * 10 + string.digits.index(c),
         s[s[0] in '+-':], 0)
 
+# My solution
+def string_to_int(s: str)-> int:
+    sign = -1 if s[0] == '-' else 1
+    s = s[1:] if s[0] in '+-' else s
+    res = 0
+    for c in s:
+        res *= 10
+        res += ord(c) - ord('0')
+    return sign * res
 
 def wrapper(x, s):
     if int(int_to_string(x)) != x:
@@ -34,3 +43,4 @@ if __name__ == '__main__':
         generic_test.generic_test_main('string_integer_interconversion.py',
                                        'string_integer_interconversion.tsv',
                                        wrapper))
+    # string_to_int("123")
